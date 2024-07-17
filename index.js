898f309802bcddf888124859e3d0a866c4bef532
Zsolt Nagy <info@zsoltnagy.eu>
2024-07-17 02:22:26 +0200

    get - api list

--- index.js
+++ index.js
@@ -11,12 +11,48 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let db = null;
+
+function dbSetup(doInsert) {
+    if (db === null) {
+        return;
+    }
+
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ShoppingList (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            item TEXT NOT NULL,
+            quantity INTEGER NOT NULL
+        );
+    `);
+
+    if (doInsert) {
+        db.run(`
+            INSERT INTO ShoppingList (item, quantity)
+            VALUES ('Eggs', 12),
+                   ('Milk', 2);
+        `);
+    }
+}
+
 function appStartedCallback() {
     console.log("App is listening");
+    db = new sqlite3.Database("shopping_list.db");
+    dbSetup(false);
 }
 
 app.listen(port, appStartedCallback);
 
 app.get("/", (req, res) => {
     res.status(200).json({ status: true });
+});
+
+app.get("/api/list", (req, res) => {
+    db.all("SELECT * FROM ShoppingList", (error, rows) => {
+        if (error) {
+            res.status(500).json({ error });
+        } else {
+            res.status(200).json(rows);
+        }
+    });
 });
