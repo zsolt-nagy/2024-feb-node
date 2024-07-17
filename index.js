ccfcb70ae61ec9f5aac0a21df54049d7aa629910
Zsolt Nagy <info@zsoltnagy.eu>
2024-07-17 02:35:32 +0200

    POST api/list/new

--- index.js
+++ index.js
@@ -56,3 +56,20 @@ app.get("/api/list", (req, res) => {
         }
     });
 });
+
+app.post("/api/list/new", (req, res) => {
+    db.run(
+        `
+        INSERT INTO ShoppingList (item, quantity)
+        VALUES (?, ?);        
+    `,
+        [req.body.item, req.body.quantity],
+        (error, response) => {
+            if (error) {
+                res.status(500).json({ error });
+            } else {
+                res.status(201).json({ response });
+            }
+        }
+    );
+});
